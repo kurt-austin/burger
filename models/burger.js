7ecdb254369a203b53e9773b0db12030fa8024b7
Kurt Austin <kurt.austin@gmail.com>
2020-08-03 19:18:14 -0500

    functioning app need css, styling, and images

--- models/burger.js
+++ models/burger.js
@@ -1,12 +1,43 @@
-const orm = require("./config/orm.js");
+const orm = require("../config/orm.js");
 
 
-orm.selectAll("burgers");
+// orm.selectAll("burgers");
 
-orm.insertOne("burgers", "burger_name","devoured","Big Mac", "True");
+// orm.insertOne("burgers", "burger_name","devoured","Big Mac", "True");
 
 
-orm.updateOne("burgers", "devoured", "false", "Big Mac");
+// orm.updateOne("burgers", "devoured", "false", "Big Mac");
+
+
+
+
+
+// Import the ORM to create functions that will interact with the database.
+// var orm = require("../config/orm.js");
+
+const burger = {
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  
+};
+
+// Export the database functions for the controller (catsController.js).
+// module.exports = cat;
+
 
 
 
